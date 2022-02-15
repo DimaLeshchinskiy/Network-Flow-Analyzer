3c8b5d75fd88471bfffb029af2d566bf111381b6
dmitryl0626 <dmitryl0626@gmail.com>
2022-02-16 00:14:55 +0100

    implement basic collector_py

---
+++ collector/src/main.py
@@ -0,0 +1,42 @@
+import pymongo
+import socket
+import json 
+
+localIP     = socket.gethostbyname(socket.gethostname())
+localPort   = 3001
+bufferSize  = 4096
+
+databaseName   = "netflow_data"
+collectionName = "data"
+
+def get_database(db_name):
+    client = pymongo.MongoClient("mongodb://root:root@collector_mongodb:27017/")
+    return client[db_name]
+
+def get_collection(db, col_name):
+    return db[col_name]
+
+if __name__ == "__main__":
+    print("After start")
+    db = get_database(databaseName)
+    collection = get_collection(db, collectionName)
+
+    print("Connected to DB")
+
+    socketServer = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+
+    # Bind to address and ip
+    socketServer.bind((localIP, localPort))
+    # Listen for incoming datagrams
+    while(True):
+        print("Listening...")
+        data = socketServer.recvfrom(bufferSize)[0]
+        dataJson = json.loads(data)
+        print(data, dataJson)
+
+        insertedIds = collection.insert_one(dataJson)
+
+        print(f"Inserted {str(insertedIds).encode()} rows.")
+
+
+
